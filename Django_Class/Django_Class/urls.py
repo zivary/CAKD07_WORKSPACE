3ec32680c6c3d3ef48f6d1e23ec79f41aeec4e44
zivary <zivary@me.com>
2022-08-03 18:42:34 +0900

    update

--- Django_Class/Django_Class/urls.py
+++ Django_Class/Django_Class/urls.py
@@ -1,4 +1,4 @@
-"""Django URL Configuration
+"""Django_Class URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
