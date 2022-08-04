93b3a685b3032addfae8e872b73e1358e1fc764c
zivary <zivary@me.com>
2022-08-04 19:39:08 +0900

    update

--- Django_Class/blog/views.py
+++ Django_Class/blog/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-# Create your views here.
+class PostList(ListView):
+    model = Post
+    # template_name = 'blog/index.html'
+    ordering = '-pk'
+
+class PostDetail(DetailView):
+    model = Post
+
+
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk')
+#     return render(
+#         request,
+#         'blog/index.html',
+#         {
+#             'post' : posts,
+#         }
+#     )s
