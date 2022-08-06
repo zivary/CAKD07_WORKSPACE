9a9670b8a5abee15edbb55f1874fb1f2821ab3b8
zivary <zivary@me.com>
2022-08-06 15:18:01 +0900

    update

--- Django_Class/blog/models.py
+++ Django_Class/blog/models.py
@@ -3,9 +3,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
-
     created_at = models.DateTimeField()
-
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
