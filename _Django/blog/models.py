19bde63dfc73dd1e30941b31711e17fd18e29f0f
zivary <zivary@me.com>
2022-08-10 00:32:37 +0900

    User 설정

--- _Django/blog/models.py
+++ _Django/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 import os
 
 # Create your models here.
@@ -12,8 +13,10 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
     def __str__(self):
-        return f'[{self.pk}]{self.title}'
+        return f'[{self.pk}]{self.title}::{self.author}'
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
