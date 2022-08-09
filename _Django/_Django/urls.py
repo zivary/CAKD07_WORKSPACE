2428ef46ff27c508ac9c22e4de9354ff3e819d29
zivary <zivary@me.com>
2022-08-09 19:14:05 +0900

    사진 올리기 파일 올리기 추가

--- _Django/_Django/urls.py
+++ _Django/_Django/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('blog/', include('blog.urls')),
@@ -22,3 +24,6 @@ urlpatterns = [
     path('', include('single_pages.urls')),
 
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
