4a37aa03d87ce3ce406a49ad467b20bc3597ef80
zivary <zivary@me.com>
2022-08-09 17:09:52 +0900

    FBV로 블로그 포스트 상세 페이지 만들기

--- _Django/blog/models.py
+++ _Django/blog/models.py
@@ -5,8 +5,11 @@ class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
 
-    Created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f'[{self.pk}]{self.title}'
+
+    def get_absolute_url(self):
+        return f'/blog/{self.pk}/'
