29fea6ff7d51f3a8d585bc0cb70cc8193a8a4c8e
zivary <zivary@me.com>
2022-08-10 01:29:37 +0900

    Category 설정

--- _Django/blog/views.py
+++ _Django/blog/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Category
 from django.views.generic import ListView, DetailView
 
 # Create your views here.
 class PostList(ListView):
     model = Post
     ordering = '-pk'
+
+    def get_context_data(self, **kwargs):
+        context = super(PostList, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        return context
+
 
 class PostDetail(DetailView):
     model = Post
