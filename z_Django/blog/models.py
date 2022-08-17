58ac2b104bf740dd8ceec52252ef4594bf7dd29f
zivary <zivary@me.com>
2022-08-18 00:44:58 +0900

    update

--- z_Django/blog/models.py
+++ z_Django/blog/models.py
@@ -65,6 +65,7 @@ class Post(models.Model):
     def get_content_markdown(self):
         return markdown(self.content)
 
+
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
--- z_Django/blog/views.py
+++ z_Django/blog/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from .models import Post, Category, Tag
+from django.shortcuts import get_object_or_404
+from .models import Post, Category, Tag, Comment
+from .forms import CommentForm
 from django.core.exceptions import PermissionDenied
 from django.utils.text import slugify
+from django.db.models import Q
 
 # Create your views here.
+
 class PostList(ListView):
     model = Post
     ordering = '-pk'
+    paginate_by = 5
 
     def get_context_data(self, **kwargs):
         context = super(PostList, self).get_context_data()
@@ -24,6 +29,7 @@ class PostDetail(DetailView):
         context = super(PostDetail, self).get_context_data()
         context['categories'] = Category.objects.all()
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        context['comment_form'] = CommentForm
         return context
 
 
@@ -101,6 +107,33 @@ class PostUpdate(LoginRequiredMixin, UpdateView):
         return response
 
 
+class CommentUpdate(LoginRequiredMixin, UpdateView):
+    model = Comment
+    form_class = CommentForm
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user == self.get_object().author:
+            return super(CommentUpdate, self).dispatch(request, *args, **kwargs)
+        else:
+            raise PermissionDenied
+
+
+class PostSearch(PostList):
+    paginate_by=None
+
+    def get_queryset(self):
+        q=self.kwargs['q']
+        post_list=Post.objects.filter(
+            Q(title__contains=q) | Q(tags__name__contains=q)
+        ).distinct()
+        return post_list
+
+    def get_context_data(self, **kwargs):
+        context = super(PostSearch, self).get_context_data()
+        q=self.kwargs['q']
+        context['search_info']=f'Search: {q} ({self.get_queryset().count()})'
+        return context
+
 
 def category_page(request, slug):
     if slug == 'no_category':
@@ -134,6 +167,33 @@ def tag_page(request, slug):
             'tag' : tag,
         }
     )
+
+
+def new_comment(request, pk):
+    if request.user.is_authenticated:
+        post = get_object_or_404(Post, pk=pk)
+
+        if request.method == 'POST':
+            comment_form = CommentForm(request.POST)
+            if comment_form.is_valid():
+                comment = comment_form.save(commit=False)
+                comment.post = post
+                comment.author = request.user
+                comment.save()
+                return redirect(comment.get_absolute_url())
+        else:
+            return redirect(post.get_absolute_url())
+    else:
+        raise PermissionDenied
+
+def delete_comment(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post = comment.post
+    if request.user.is_authenticated and request.user == comment.author:
+        comment.delete()
+        return redirect(post.get_absolute_url())
+    else:
+        raise PermissionDenied
 
 
 # def index(request):
